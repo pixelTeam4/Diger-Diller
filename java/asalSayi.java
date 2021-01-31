2a6c44a1896a6a3505c9feccf2beba500e7d94ca
ozbakirsahincan <77192366+ozbakirsahincan@users.noreply.github.com>
2021-01-31 23:24:17 +0300

    Create asalSayi.java

---
+++ java/asalSayi.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int number=3;
+        boolean isPrime = true;
+        for (int i=2;i<number;i++){
+            if (number%i==0){
+                isPrime=false;
+            }
+        }
+        if (isPrime){
+            System.out.println("Sayı Asal Sayıdır");
+        }
+        else{
+            System.out.println("Sayı asal değildir.");
+        }
+    }
+}
